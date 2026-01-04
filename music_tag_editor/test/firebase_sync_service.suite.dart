e28e45cc74690ee263d455de9f4a804e2338450d
João Magdaleno <joaomagdaleno.14@hotmail.com>
2026-01-04 11:31:12 -0300

    test: add Firebase sync service unit tests and Gradle wrapper configuration for Gradle 8.11.1

--- music_tag_editor/test/firebase_sync_service.suite.dart
+++ music_tag_editor/test/firebase_sync_service.suite.dart
@@ -7,6 +7,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:music_tag_editor/services/firebase_sync_service.dart';
 import 'package:music_tag_editor/services/database_service.dart';
+import 'package:music_tag_editor/widgets/learning_dialog.dart';
 
 class MockFirebaseAuth extends Mock implements FirebaseAuth {}
 
@@ -47,6 +48,12 @@ void main() {
     registerFallbackValue(MockDocumentReference());
     registerFallbackValue(SetOptions(merge: true));
     registerFallbackValue({}); // for data map
+    registerFallbackValue(LearningRule(
+      field: '',
+      originalValue: '',
+      correctedValue: '',
+      choice: LearningChoice.justThisOnce,
+    ));
   });
 
   setUp(() {
